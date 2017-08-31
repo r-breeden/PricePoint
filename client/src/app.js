a194a5a107a2c85d74c1bd61621d72b010bb7ea1
kiyeonpark <kiyeon.c.park@gmail.com>
2017-08-31 16:19:47 -0700

    Consolidate redux changes with the html held by the constant app

--- client/src/app.js
+++ client/src/app.js
@@ -3,8 +3,37 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store';
 
+const App = (
+  <div className="price-point-app">
+    <div className="container">
+      <Row>
+        <Col md={6}>
+          <Navbar fixedTop fluid>
+            <Navbar.Header>
+              <Navbar.Brand>
+                <a href="/">PricePoint</a>
+                <small> a powerful price tracker tool</small>
+              </Navbar.Brand>
+            </Navbar.Header>
+            <Navbar.Text pullRight>
+              <Button>
+                <a href='/login'><Glyphicon glyph="log-in"/></a>
+              </Button>
+            </Navbar.Text>
+          </Navbar>
+          {/* <Search>
+          </Search> */}
+        </Col>
+      </Row>
+    </div>
+  </div>
+);
+
 ReactDOM.render(
   <Provider store={store}>
-    <h1>Hello World from React</h1>
+    {App}
   </Provider>,
   document.getElementById('root'));
+
+
+
