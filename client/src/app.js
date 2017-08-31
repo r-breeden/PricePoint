e9ac640e9ad0f7167d1b4fe806391c11c43b5996
Ian Mobley <ianmobley@gmail.com>
2017-08-31 15:38:37 -0700

    add Provider wrapper and store

--- client/src/app.js
+++ client/src/app.js
@@ -1,4 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 
-ReactDOM.render(<h1>Hello World from React</h1>, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <h1>Hello World from React</h1>
+  </Provider>,
+  document.getElementById('root'));
