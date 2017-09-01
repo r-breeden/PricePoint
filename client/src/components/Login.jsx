ee4c9aa2d9948559d8c1cf32494d3008a492fae1
kiyeonpark <kiyeon.c.park@gmail.com>
2017-09-01 16:51:28 -0700

    Add links and icons to oAuth routes (facebook, google, and twitter)

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -3,6 +3,22 @@ import ReactDOM from 'react-dom';
 import { Grid, Row, Col } from 'react-bootstrap'; 
 import { Glyphicon, Form, FormGroup, ControlLabel, FormControl, Button } from 'react-bootstrap';    
 
+const fblogo = {
+  width: '29px', 
+  height: '29px', 
+  marginRight: '15px'
+};
+const googlogo = {
+  width: '29px', 
+  height: '29px', 
+  marginLeft: '15px', 
+  marginRight: '15px'
+};
+const twitlogo = {
+  width: '48px', 
+  height: '48px', 
+  marginLeft: '15px'
+};
 
 const Login = () => {
   return (
@@ -44,9 +60,21 @@ const Login = () => {
               </FormGroup>            
               <Button bsSize="large" bsStyle="warning">Login</Button>
             </Form>
+            <div>
+              <br/>
+              Or login with any of the following services: 
+              <br/><br/>
+              <a href="/auth/facebook">
+                <img style={fblogo} src="/assets/fb-logo.png"/>
+              </a>
+              <a href="/auth/google">
+                <img style={googlogo} src="/assets/google-logo.png"/>
+              </a>
+              <a href="/auth/twitter">
+                <img style={twitlogo} src="/assets/twitter-logo.png"/>
+              </a>
+            </div>
           </Col>
-          
-
         </Row>
       </Grid>
     </div>
@@ -55,15 +83,7 @@ const Login = () => {
 
 export default Login; 
 
-//         <hr>
-//         <div>
-//             Or login with any of the following services:<br />
-//             <a style="margin-right:15px" href="/auth/facebook"><img src="/assets/fb-logo.png" /></a>
-//             <a style="margin-left:15px;margin-right:15px" href="/auth/google"><img style="width:29px" src="/assets/google-logo.png" /></a>
-//             <a style="margin-left:15px" href="/auth/twitter"><img style="width:48px" src="/assets/twitter-logo.png" /></a>
-//         </div>
-//         <hr>
-// 
+
 //         <p>Need to sign up for an account? <a href="/signup">Signup</a></p>
 //         <p><a href="/">home</a></p>
 // 
