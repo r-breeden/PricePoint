9418063b9fc64c657b136ffc110cfd0b80474892
kiyeonpark <kiyeon.c.park@gmail.com>
2017-08-31 19:11:37 -0700

    Create a search input field and submit button

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Grid, Row, Col } from 'react-bootstrap'; 
+import { Form, FormGroup, FormControl, Button } from 'react-bootstrap'; 
 
-
-const Search = (
-  <h1>meow</h1>
-);
+const Search = () => {
+  return (
+    <Form inline>
+      <FormGroup>
+        <FormControl
+          type="text"
+          placeholder="Search for an item"
+        />
+      </FormGroup>
+      {' '}
+      <Button>
+        Submit
+      </Button>
+    </Form>
+  );
+};
+  
 
 export default Search; 
