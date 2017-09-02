8a5dae81a8f3b0d9efc3d4b2d87861d42ade1812
Ryan Breeden <ryan@ryanbreeden.com>
2017-09-01 18:58:19 -0700

    amazon normalize function added

--- server/helper.js
+++ server/helper.js
@@ -1,40 +1,40 @@
-var ignoreTypeError = function (cb){
-  try{
+var ignoreTypeError = function (cb) {
+  try {
     cb();
-  } catch(e) {
+  } catch (e) {
     if (e instanceof TypeError) {
       console.log('ignored error: ', e);
     } else {
       throw e;
     }
   }
-}
+};
 
 var normalizeAmazon = function (obj) {
   var normalized = {};
 
   normalized.title = '';
-  ignoreTypeError( () => {normalized.title = obj.ItemAttributes[0].Title[0]; } )
+  ignoreTypeError( () => { normalized.title = obj.ItemAttributes[0].Title[0]; } );
 
   normalized.itemURL = '';
-  ignoreTypeError( () => { normalized.itemURL = obj.DetailPageURL[0]; })
+  ignoreTypeError( () => { normalized.itemURL = obj.DetailPageURL[0]; });
 
   normalized.imageURL = '';
-  ignoreTypeError( () => { normalized.imageURL = obj.LargeImage[0].URL[0]; })
+  ignoreTypeError( () => { normalized.imageURL = obj.LargeImage[0].URL[0]; });
 
   normalized.description = '';
-  ignoreTypeError( () => { normalized.description = obj.ItemAttributes[0].Feature[0]; })
+  ignoreTypeError( () => { normalized.description = obj.ItemAttributes[0].Feature[0]; });
 
   normalized.price = '';
-  ignoreTypeError( () => { normalized.price = obj.OfferSummary[0].LowestNewPrice[0].FormattedPrice[0]; })
+  ignoreTypeError( () => { normalized.price = obj.OfferSummary[0].LowestNewPrice[0].FormattedPrice[0]; });
 
   normalized.upc = '';
-  ignoreTypeError( () => { normalized.upc = obj.ItemAttributes[0].UPC[0]; })
+  ignoreTypeError( () => { normalized.upc = obj.ItemAttributes[0].UPC[0]; });
 
   normalized.brand = '';
-  ignoreTypeError( () => { normalized.brand = obj.ItemAttributes[0].Brand[0]; })
+  ignoreTypeError( () => { normalized.brand = obj.ItemAttributes[0].Brand[0]; });
 
   return normalized;
-}
+};
 
 module.exports = normalizeAmazon;
