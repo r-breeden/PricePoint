bb0831acdfb3ffd0ba93298a679ab4e80e693450
theenderweggin <jrstieber@gmail.com>
2017-09-01 17:23:04 -0700

    Remove comments

--- db/migrations/20170831170312_new_schema.js
+++ db/migrations/20170831170312_new_schema.js
@@ -2,23 +2,8 @@
 exports.up = function(knex, Promise) {
   return Promise.all([
     knex.schema.table('profiles', function (table) {
-      // table.increments('id').unsigned().primary();
-      // table.string('first', 100).nullable();
-      // table.string('last', 100).nullable();
-      // table.string('display', 100).nullable();
-      // table.string('email', 100).nullable().unique();
-      // table.string('phone', 100).nullable();
       table.string('photo_path', 100).nullable();
-      // table.timestamps(true, true);
     }),
-    // knex.schema.createTableIfNotExists('auths', function(table) {
-    //   table.increments('id').unsigned().primary();
-    //   table.string('type', 8).notNullable();
-    //   table.string('oauth_id', 30).nullable();
-    //   table.string('password', 100).nullable();
-    //   table.string('salt', 100).nullable();
-    //   table.integer('profile_id').references('profiles.id').onDelete('CASCADE');
-    // }),
     knex.schema.createTableIfNotExists('vendors', function(table) {
       table.increments('id').unsigned().primary();
       table.string('name',100).notNullable();
@@ -53,9 +38,6 @@ exports.up = function(knex, Promise) {
 
 exports.down = function(knex, Promise) {
   return Promise.all([
-    // knex.schema.dropTable('auths'),
-    // knex.schema.dropTable('profiles'),
-    //
     knex.schema.table('profiles', (table) => {
       table.dropColumn('photo_path');
     }),
