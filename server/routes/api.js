aec8980866cdcfa058b50ee7e4410c99f4c7124a
Ryan Breeden <ryan@ryanbreeden.com>
2017-09-01 19:17:12 -0700

    normalize amazon api

--- server/routes/api.js
+++ server/routes/api.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 const amazonAuth = require('../../config/development.json').amazon;
 let amazon = require('amazon-product-api');
-let helper = require('../helper');
+let helper = require('../helpers/helpers.js');
 
 //setup for amazon-product-api 
 //for use with amazon api query
@@ -22,19 +22,5 @@ router.route('/')
     console.log('in the correct route');
     res.status(201).send({ data: 'Posted!' });
   });
-
-router.route('/search')
-  .get((req, res) => {
-    client.itemSearch({
-      searchIndex: 'Electronics',
-      keywords: 'tv',
-      responseGroup: 'ItemAttributes,Offers,Images'
-    }).then(function(results){
-      return results;
-    }).catch(function(err){
-      console.log('amazon query error: ', err);
-    });
-  });
-
 
 module.exports = router;
