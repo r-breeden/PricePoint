3b8fc22201407fe776a9ed5ce8eb78b6b67b9674
Ian Mobley <ianmobley@gmail.com>
2017-09-05 09:42:57 -0700

    Rename router reducer

--- client/src/store/reducers/index.js
+++ client/src/store/reducers/index.js
@@ -1,10 +1,9 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
-// get routerReducer
 
 import currentUser from './currentUser';
 
 export default combineReducers({
-  routerReducer,
+  router: routerReducer,
   currentUser,
 });
