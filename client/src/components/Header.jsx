bdb0f7561eceab776199b4f8e83143b5b63adac1
kiyeonpark <kiyeon.c.park@gmail.com>
2017-09-05 19:38:42 -0700

    remove unecessary component imports

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Grid, Row, Col } from 'react-bootstrap';
 import { Navbar, Button, Glyphicon } from 'react-bootstrap';
-import Search from './Search.jsx';
-import Results from './Results.jsx';
 import Login from './Login.jsx';
 
 const Header = () => {
