7f30e672dd9578d681c1d17c9a4e954761ffb846
Ian Mobley <ianmobley@gmail.com>
2017-09-05 19:10:05 -0700

    export all current models

--- db/models/index.js
+++ db/models/index.js
@@ -1,2 +1,9 @@
-module.exports.Auth = require('./auths');
-module.exports.Profile = require('./profiles');
+module.exports = {
+  Auth: require('./auths'),
+  Price: requrie('./prices'),
+  Product: require('./products'),
+  ProductUrl: require('./product_url'),
+  Profile: require('./profiles'),
+  Vendor: require('./vendors'),
+};
+
