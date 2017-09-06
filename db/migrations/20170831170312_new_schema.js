8daeab6dd750953f337af405bdcefa5f4c05d163
Ian Mobley <ianmobley@gmail.com>
2017-09-06 16:13:31 -0700

    Update snapshot, price type, profile add product

--- db/migrations/20170831170312_new_schema.js
+++ db/migrations/20170831170312_new_schema.js
@@ -7,12 +7,12 @@ exports.up = function(knex, Promise) {
     knex.schema.createTableIfNotExists('vendors', function(table) {
       table.increments('id').unsigned().primary();
       table.string('name', 100).notNullable();
-      table.string('url', 100).notNullable();
+      table.string('url', 100).unique().notNullable();
     }),
     knex.schema.createTableIfNotExists('products', function(table) {
       table.increments('id').unsigned().primary();
       table.string('name', 100).notNullable();
-      table.string('upc', 100).notNullable();
+      table.string('upc', 100).unique().notNullable();
       table.string('description', 255).notNullable();
     }),
     knex.schema.createTableIfNotExists('product_urls', function(table) {
@@ -25,11 +25,10 @@ exports.up = function(knex, Promise) {
       table.increments('id').unsigned().primary();
       table.integer('vendor_id').references('vendors.id').onDelete('CASCADE');
       table.integer('product_id').references('products.id').onDelete('CASCADE');
-      table.decimal('price', 15, 2).notNullable();
-      table.timestamps(true);
+      table.decimal('price', 14, 2).notNullable();
+      table.timestamps();
     }),
     knex.schema.createTableIfNotExists('followed_products', function(table) {
-      table.increments('id').unsigned().primary();
       table.integer('profile_id').references('profiles.id').onDelete('CASCADE');
       table.integer('product_id').references('products.id').onDelete('CASCADE');
     })
