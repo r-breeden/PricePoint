9fc43f13fd9d5438d6274035e64d6fb840a64a7b
kiyeonpark <kiyeon.c.park@gmail.com>
2017-09-05 19:36:11 -0700

    index pointing temporarily for testing purposes

--- client/src/Index.jsx
+++ client/src/Index.jsx
@@ -1,35 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Grid, Row, Col } from 'react-bootstrap'; 
-import { Navbar, Button, Glyphicon } from 'react-bootstrap'; 
-import Search from './components/Search.jsx'; 
+import { Grid, Row, Col } from 'react-bootstrap';
+import { Navbar, Button, Glyphicon } from 'react-bootstrap';
+import Search from './components/Search.jsx';
 import Results from './components/Results.jsx';
-import Login from './components/Login.jsx'; 
+import Login from './components/Login.jsx';
+import Header from './components/Header.jsx';
+import Profile from './components/Profile.jsx'; 
 
 const Index = () => {
   return (
-    <Grid fluid>
-      <Row>
-        <Col md={12}>
-          <Navbar fixedTop fluid>
-            <Navbar.Header>
-              <Navbar.Brand>
-                <a href="/">PricePoint</a>
-                <small> a powerful price tracker tool</small>
-              </Navbar.Brand>
-            </Navbar.Header>
-            <Navbar.Text pullRight>
-              <Button>
-                <a href='/login'><Glyphicon glyph="log-in"/></a>
-              </Button>
-            </Navbar.Text>
-          </Navbar>
-        </Col>
-      </Row>
-      <Search></Search>
-      <Results></Results>
+    <Grid>
+      <Profile></Profile>
     </Grid>
-  ); 
-}; 
+  );
+};
 
-export default Index; 
+export default Index;
