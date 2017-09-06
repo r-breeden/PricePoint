7d07bd4eb6a5a1e99cd8ae484aa25c82b27a02a8
Ian Mobley <ianmobley@gmail.com>
2017-09-06 16:26:39 -0700

    Revert "remove leftover helper folder (moved?)"
    This reverts commit 857a4531e26700eb4153a52a04c60ca57bc80667.

---
+++ server/helper/helpers.js
@@ -0,0 +1,40 @@
+var ignoreTypeError = function (cb) {
+  try {
+    cb();
+  } catch (e) {
+    if (e instanceof TypeError) {
+      console.log('ignored error: ', e);
+    } else {
+      throw e;
+    }
+  }
+};
+
+var normalizeAmazon = function (obj) {
+  var normalized = {};
+
+  normalized.title = '';
+  ignoreTypeError( () => { normalized.title = obj.ItemAttributes[0].Title[0]; } );
+
+  normalized.itemURL = '';
+  ignoreTypeError( () => { normalized.itemURL = obj.DetailPageURL[0]; });
+
+  normalized.imageURL = '';
+  ignoreTypeError( () => { normalized.imageURL = obj.LargeImage[0].URL[0]; });
+
+  normalized.description = '';
+  ignoreTypeError( () => { normalized.description = obj.ItemAttributes[0].Feature[0]; });
+
+  normalized.price = '';
+  ignoreTypeError( () => { normalized.price = obj.OfferSummary[0].LowestNewPrice[0].FormattedPrice[0]; });
+
+  normalized.upc = '';
+  ignoreTypeError( () => { normalized.upc = obj.ItemAttributes[0].UPC[0]; });
+
+  normalized.brand = '';
+  ignoreTypeError( () => { normalized.brand = obj.ItemAttributes[0].Brand[0]; });
+
+  return normalized;
+};
+
+module.exports = normalizeAmazon;
