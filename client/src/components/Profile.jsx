33aa7ca9221cdae3782021bb88bdb4cdec329bac
kiyeonpark <kiyeon.c.park@gmail.com>
2017-09-07 10:45:17 -0700

    dynamically render user information // work in progress

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -4,17 +4,20 @@ import { Grid, Row, Col } from 'react-bootstrap';
 import { Thumbnail, Table, Checkbox, Button } from 'react-bootstrap';
 import Header from './Header.jsx';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import '../styles/main.scss';
 
-const Profile = () => {
+const Profile = (props) => {
+  console.log(props);
   return (
     <Grid>
       <Header></Header>
       <Row>
         <Col xs={6} md={3}>
-          <Thumbnail href="#" alt="171x180" src="http://rougier.github.io/bootstrap-rst/171x180.png" />
+          <Thumbnail className="profileavi" href="#" alt="171x180" src={`${props.user.userphoto}`} />
         </Col>
         <Col xs={6} md={9}>
-          <h2>Username</h2>
+          <h2>{props.user.username}</h2>
           <a href='/logout'>Log out</a> | <Link to='/'>Home</Link>
         </Col>
       </Row>
@@ -28,26 +31,15 @@ const Profile = () => {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td><Link to="/product">Item 1</Link></td>
-              <td>$$</td>
-              <td><Checkbox></Checkbox></td>
-            </tr>
-            <tr>
-              <td><Link to="/product">Item 2</Link></td>
-              <td>$$</td>
-              <td><Checkbox></Checkbox></td>
-            </tr>
-            <tr>
-              <td><Link to="/product">Item 3</Link></td>
-              <td>$$</td>
-              <td><Checkbox></Checkbox></td>
-            </tr>
-            <tr>
-              <td><Link to="/product">Item 4</Link></td>
-              <td>$$</td>
-              <td><Checkbox></Checkbox></td>
-            </tr>
+            {props.user.followed.map((el, i) => {
+              return (
+                <tr>
+                  <td><Link to="/product">{el.item}</Link></td>
+                  <td>{el.lowestprice}</td>
+                  <td><Checkbox checked="true"></Checkbox></td>
+                </tr>
+              );
+            })}
             <tr>
               <td></td>
               <td></td>
@@ -60,6 +52,12 @@ const Profile = () => {
   );
 };
 
+const mapStateToProps = (state) => {
+  return {
+    'user': state.currentUser
+  };
+};
 
+export const UnwrappedProfile = Profile;
 
-export default Profile;
+export default connect(mapStateToProps)(Profile);
