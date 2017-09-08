a0ccc83802f5b4cd208ade74661499078c542211
kiyeonpark <kiyeon.c.park@gmail.com>
2017-09-07 19:18:31 -0700

    separate any inline styling, add missing dependencies for bootstrap

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -2,63 +2,81 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Grid, Row, Col } from 'react-bootstrap';
 import {
-  Button, Glyphicon,
+  Button, Glyphicon, PageHeader,
   Form, FormGroup, ControlLabel, FormControl,
 } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import Header from './Header.jsx';
 
-const Login = () => (
-  <Grid>
-    <Header></Header>
-    <Row>
-      <Col sm={6} smOffset={3}>
-        <h1>
-          <span>
-            <Glyphicon glyph="log-in" bsSize="small"></Glyphicon> Login
-          </span>
-        </h1>
-        <Form action="/login" method="post">
-          <FormGroup>
-            <ControlLabel>
-              Email
-            </ControlLabel>
-            <FormControl
-              type="text"
-              name="email"
-            />
-          </FormGroup>
-          <FormGroup>
-            <ControlLabel>
-              Password
-            </ControlLabel>
-            <FormControl
-              type="password"
-              name="password"
-            />
-          </FormGroup>
-          <Button type="submit" bsSize="large" bsStyle="warning">Login</Button>
-        </Form>
-        <hr />
-        <div>
-          Or login with any of the following services:
-          <br/>
-          <a style={{marginRight: 15}} href="/auth/facebook">
-            <img src="/assets/fb-logo.png" />
-          </a>
-          <a style={{marginLeft: 15, marginRight: 15}} href="/auth/google">
-            <img style={{width: 29}} src="/assets/google-logo.png" />
-          </a>
-          <a style={{marginLeft: 15}} href="/auth/twitter">
-            <img style={{width: 48}} src="/assets/twitter-logo.png" />
-          </a>
-        </div>
-        <hr />
-        <p>Need to sign up for an account? <Link to="/signup">Signup</Link></p>
-        <p><Link to="/">home</Link></p>
-      </Col>
-    </Row>
-  </Grid>
-);
+const Login = () => {
+  return (
+    <div>
+      <Grid>
+        <Row>
+          <Col sm={6} smOffset={3}>
+            <h1>
+              <span>
+                Login
+              </span>
+            </h1>
+            {/* some conditional rendering logic:
+            if (message.length) { %>
+            <div class="alert alert-danger"><%= message %></div>
+            <% } */}
+            <Form>
+              <FormGroup controlId="formBasicText">
+                <ControlLabel>
+                  Email
+                </ControlLabel>
+                <FormControl
+                  className="signup-input"
+                  type="text"
+                  name="email"
+                />
+                <FormControl.Feedback />
+              </FormGroup>
+              <FormGroup controlId="formBasicText">
+                <ControlLabel>
+                  Password
+                </ControlLabel>
+                <FormControl
+                  className="signup-input"
+                  type="password"
+                  name="password"
+                />
+                <FormControl.Feedback />
+              </FormGroup>
+              <Button bsSize="large" bsStyle="warning">Login</Button>
+            </Form>
+            <div>
+              <PageHeader></PageHeader>
+              Or login with any of the following services:
+              <br/>
+              <a href="/auth/facebook">
+                <img className="fblogo" src="/assets/facebook.svg"/>
+              </a>
+              <a href="/auth/google">
+                <img className="googlogo" src="/assets/google.svg"/>
+              </a>
+              <a href="/auth/twitter">
+                <img className="twitlogo" src="/assets/twitter.svg"/>
+              </a>
+            </div>
+            <PageHeader></PageHeader>
+            <div>
+              <p>
+                Need to sign up for an account?
+                <Link to="/signup"> Signup</Link>
+              </p>
+              <p>
+                <Link to="/">home</Link>
+              </p>
+            </div>
+          </Col>
+        </Row>
+      </Grid>
+    </div>
+  );
+};
 
 export default Login;
