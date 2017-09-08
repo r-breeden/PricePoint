50d42830a8afd09b3f98318543992ac5cc90a373
kiyeonpark <kiyeon.c.park@gmail.com>
2017-09-08 16:07:37 -0700

    import link from react-router, login/signup works correctly

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Link } from 'react-router-dom';
 import { Grid, Row, Col } from 'react-bootstrap';
 import { Navbar, Nav, NavItem, Button, Glyphicon } from 'react-bootstrap';
-import Login from './Login.jsx';
 
 const Header = () => {
   return (
@@ -14,7 +14,6 @@ const Header = () => {
               <Navbar.Brand>
                 <img className="logo" src="https://image.flaticon.com/icons/svg/534/534884.svg" />
                 <a className="brand-name" href="/">PricePoint</a>
-
                 <small className="blurb"> a powerful price tracking tool</small>
               </Navbar.Brand>
               <Navbar.Toggle />
@@ -23,8 +22,16 @@ const Header = () => {
               <Nav>
               </Nav>
               <Nav pullRight>
-                <NavItem className="menu-items" eventKey={1} href="/login"><Glyphicon glyph="log-in"/> Log In</NavItem>
-                <NavItem className="menu-items" eventKey={2} href="/signup"><Glyphicon glyph="user"/> Sign Up</NavItem>
+                <NavItem className="menu-items" eventKey={1}>
+                  <Link to="/login">
+                    <Glyphicon glyph="log-in"/> Log In
+                  </Link>
+                </NavItem>
+                <NavItem className="menu-items" eventKey={2}>
+                  <Link to="/signup">
+                    <Glyphicon glyph="user"/> Sign Up
+                  </Link>
+                </NavItem>
               </Nav>
             </Navbar.Collapse>
           </Navbar>
