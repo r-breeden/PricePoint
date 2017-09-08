3f33ea8fcdb2f322b342726f0d130fe26edd4832
kiyeonpark <kiyeon.c.park@gmail.com>
2017-09-08 15:05:22 -0700

    render profile for visual testing purposes

--- client/src/Index.jsx
+++ client/src/Index.jsx
@@ -5,11 +5,12 @@ import { Navbar, Button, Glyphicon } from 'react-bootstrap';
 import Search from './components/Search.jsx';
 import Results from './components/Results.jsx';
 
+import Profile from './components/Profile.jsx'; 
+
 const Index = () => {
   return (
     <Grid>
-      <Search></Search>
-      <Results></Results>
+      <Profile></Profile>
     </Grid>
   );
 };
