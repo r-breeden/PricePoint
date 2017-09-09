274cea798e22fa21b22038961937f6d39c9b923f
kiyeonpark <kiyeon.c.park@gmail.com>
2017-09-09 15:47:27 -0700

    Update auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -9,6 +9,7 @@ router.route(['/', '/profile'])
     var state = {
       user: req.user, // get the user out of session and pass to template
       results: dummyData,
+      
     };
     state.user.watchList = [
       {item: 'Nintendo Switch Gaming Console with Gray Joy-Con', lowestPrice: '$1.00'},
