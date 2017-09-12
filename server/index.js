cc776c26c727be4d241ffa4b988d3699bd87b544
Ian Mobley <ianmobley@gmail.com>
2017-09-11 18:42:52 -0700

    run migration before starting the server

--- server/index.js
+++ server/index.js
@@ -3,7 +3,8 @@ const app = require('./app');
 const db = require('../db');
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log('Example app listening on port 3000!');
-});
+db.knex.migrate.latest()
+  .then(() => app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}!`);
+  }));
 
