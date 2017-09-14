40ca232fc76f6158c5c3dbda98fecb3909dd1399
kiyeonpark <kiyeon.c.park@gmail.com>
2017-09-13 19:48:35 -0700

    Move col into a row, fix small visual error

--- client/src/components/Results.jsx
+++ client/src/components/Results.jsx
@@ -1,18 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Col, Row } from 'react-bootstrap';
 import ResultsEntries from './ResultsEntries.jsx';
 
-const Results = () => {
-  return (
-    <div>
-      <Col xs={12} md={6} lg={4}>
-        <Row>
-          <ResultsEntries></ResultsEntries>
-        </Row>
-      </Col>
-    </div>
-  );
-};
+const Results = () => (
+  <div>
+    <ResultsEntries></ResultsEntries>
+  </div>
+);
 
 export default Results;
