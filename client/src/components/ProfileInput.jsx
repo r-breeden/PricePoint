052ef912774a0eb3de00ed988b59f5147f27ba28
kiyeonpark <kiyeon.c.park@gmail.com>
2017-09-13 17:28:06 -0700

    Initialize input field to allow users to add their own categories - import and pass in necessary actions - create onSubmit handler for the Form tag - initialize input variable and inputRef for the input to hold reference to user input - use connect to link state and dispatch to component

--- client/src/components/ProfileInput.jsx
+++ client/src/components/ProfileInput.jsx
@@ -2,18 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Row, Col } from 'react-bootstrap';
 import { Form, FormGroup, FormControl } from 'react-bootstrap';
+import { connect } from 'react-redux';
+import { createTable } from '../store/actions/tables';
+import { bindActionCreators } from 'redux';
 import '../styles/main.scss';
 
-const ProfileInput = () => {
+const ProfileInput = ({ createTable }) => {
+  let input;
   return (
     <Row>
-      <Form>
+      <Form
+        onSubmit={e => {
+          e.preventDefault();
+          createTable(input.value);
+          input.value = '';
+        }}
+      >
         <FormGroup
           controlId="formBasicText"
         >
           <FormControl
             type="text"
             placeholder="Create a New Category"
+            inputRef={ ref => {
+              input = ref;
+            }}
           />
           <FormControl.Feedback />
         </FormGroup>
@@ -22,4 +35,10 @@ const ProfileInput = () => {
   );
 };
 
-export default ProfileInput;
+const mapStateToProps = state => ({
+  tables: state.tables
+});
+const mapDispatchToProps = dispatch => bindActionCreators({
+  createTable: createTable,
+}, dispatch);
+export default connect(mapStateToProps, mapDispatchToProps)(ProfileInput);
