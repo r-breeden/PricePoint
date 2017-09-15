f4ecf474eb9841a41861c68b1b924b0ee1593a8c
Ian Mobley <ianmobley@gmail.com>
2017-09-15 16:49:03 -0700

    Add cron and update least recent prices every 15 minutes

--- server/index.js
+++ server/index.js
@@ -1,7 +1,15 @@
 'use strict';
+const { CronJob } = require('cron');
 const app = require('./app');
 const db = require('../db');
+const { Products: productController } = require('./controllers');
 const PORT = process.env.PORT || 3000;
+
+new CronJob({
+  cronTime: '0 */15 * * * *',
+  onTick: productController.updateLeastRecent,
+  start: true,
+});
 
 db.knex.migrate.latest()
   .then(() => app.listen(PORT, () => {
