19058275d3fc7918a2b7c19f8743d5a345af1ef7
theenderweggin <jrstieber@gmail.com>
2017-09-15 20:27:51 -0700

    added reload feature to product pages

--- server/app.js
+++ server/app.js
@@ -38,20 +38,22 @@ app.use('/product/:upc', (req, res) => {
       return product.serializeWithPrices()
         .then(productWithPrices => {
           state.results = [productWithPrices];
+        })
+        .then( ()=> {
+          state.user = req.user;
+          state.tables = {
+            default: [
+              {item: 'Nintendo Switch Gaming Console with Gray Joy-Con', lowestPrice: '$1.00'},
+              {item: 'Intel CM8066201919901 OEM Core i7-6700K Skylake Processor 4.0 GHz 8.0GTs-8MB LGA 1151 CPU', lowestPrice: '$1.00'},
+              {item: 'Predator XB281HK 28" 3840x2160 LED Monitor', lowestPrice: '$1.00'},
+              {item: 'Samsung 65" Class 4K (2160P) Smart QLED TV', lowestPrice: '$1.00'},
+              {item: 'Viaboot Raspberry Pi 3 Complete Kit with Premium Black Case', lowestPrice: '$1.00'}
+            ]};
+          res.render('index', {state});
         });
-      state.user = req.user;
-      state.tables = {
-        default: [
-          {item: 'Nintendo Switch Gaming Console with Gray Joy-Con', lowestPrice: '$1.00'},
-          {item: 'Intel CM8066201919901 OEM Core i7-6700K Skylake Processor 4.0 GHz 8.0GTs-8MB LGA 1151 CPU', lowestPrice: '$1.00'},
-          {item: 'Predator XB281HK 28" 3840x2160 LED Monitor', lowestPrice: '$1.00'},
-          {item: 'Samsung 65" Class 4K (2160P) Smart QLED TV', lowestPrice: '$1.00'},
-          {item: 'Viaboot Raspberry Pi 3 Complete Kit with Premium Black Case', lowestPrice: '$1.00'}
-        ]};
-      res.render('index', {state});
-      // res.status(200).send(product);
     })
     .error(err => {
+      console.log(err);
       res.status(500).send(err);
     })
     .catch(() => {
