d48a43b0405328612b5e896ec8e0290fa08c015b
kiyeonpark <kiyeon.c.park@gmail.com>
2017-09-18 19:50:44 -0700

    correct parameters being passed in

--- server/controllers/categories.js
+++ server/controllers/categories.js
@@ -4,22 +4,34 @@ const models = require('../../db/models');
 module.exports.addCategory = (user, table) => {
   return models.Categories.forge({
     name: table,
-    profile_id: user.id,
+    profile_id: user,
   }).save();
 };
 
 module.exports.addItem = (user, table, upc) => {
-  //search item by upc, get product id
-  //find category id is by user id and table name
-  //add category id and product id to categories_products
-  return models.Categories.forge({
-    
-  });
+  return models.Product.where({
+    upc: upc
+  })
+    .then(product => {
+      let productId = product.serialize().id;
+      return models.Categories.forge({
+        profile_id: user,
+        name: table
+      }).products().attach(productId);
+    });
 };
 
-module.exports.retrieveCategory = () => {
+module.exports.retrieveCategory = (user) => {
   // given user id from profile, return each category with each item within category
+  return models.Categories.where({
+    profile_id: user
+  }).fetch()
+    .then(categories => {
+      return categories.serialize();
+    });
 };
+
+
 
 var retrieveProductID = () => {
   //search item by upc, get product id
