39877a9ef3e5222af3f1ac59526102651fb7aea4
Ryan Breeden <ryan@ryanbreeden.com>
2017-09-18 19:20:59 -0700

    call to db to store new table implemented, tables currently uses dummy data

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -5,15 +5,32 @@ import ProfileUser from './ProfileUser.jsx';
 import ProfileInput from './ProfileInput.jsx';
 import ProfileTable from './ProfileTable.jsx';
 import '../styles/main.scss';
+import { connect } from 'react-redux';
+
 
 const Profile = () => {
+  var dummyData = [
+    {name: 'tableOne', listItems: [{upc: '887276197470'}, {upc: '887276197470'}, {upc: '887276197470'}]},
+    {name: 'tableTwo', listItems: [{upc: '887276197470'}, {upc: '887276197470'}, {upc: '887276197470'}]},
+    {name: 'tableThree', listItems: [{upc: '887276197470'}, {upc: '887276197470'}, {upc: '887276197470'}]},
+  ]
+ 
   return (
     <Grid>
       <ProfileUser></ProfileUser>
       <ProfileInput></ProfileInput>
-      <ProfileTable></ProfileTable>
+      {dummyData.map( (list, i) => {
+        return <ProfileTable key={i} name={list.name} items={list.listItems}></ProfileTable>
+      })}
     </Grid>
   );
 };
 
-export default Profile;
+const mapStateToProps = state => {
+  return {
+    'tables': state.tables,
+  };
+};
+
+export default connect(mapStateToProps)(Profile);
+
