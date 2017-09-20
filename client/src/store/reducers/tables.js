15c0cec60a5975f56eaff59959178a188c027172
kiyeonpark <kiyeon.c.park@gmail.com>
2017-09-20 15:13:23 -0700

    add update table to push item at current index list, return state

--- client/src/store/reducers/tables.js
+++ client/src/store/reducers/tables.js
@@ -4,12 +4,14 @@ export default (state = [], action) => {
   switch (action.type) {
   case constants.CREATE_TABLE:
     return [...state, action.payload];
-
-  // case constants.UPDATE_TABLE:
-  //   return {
-  //     ..state,
-  //
-  //   }
+  case constants.UPDATE_TABLE:
+    console.log(state);
+    console.log('index', action.index);
+    console.log('payload', action.payload);
+    state[action.index].list.push(action.payload);
+    return [
+      ...state
+    ];
   default:
     return state;
   }
