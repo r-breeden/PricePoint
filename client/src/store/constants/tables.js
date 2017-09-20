1ad54b53d4094b04547f87d8fa1e917c15e69683
kiyeonpark <kiyeon.c.park@gmail.com>
2017-09-20 16:09:41 -0700

    add delete item and delete table constant

--- client/src/store/constants/tables.js
+++ client/src/store/constants/tables.js
@@ -1,3 +1,4 @@
 export const CREATE_TABLE = 'table/CREATE_TABLE';
 export const DELETE_TABLE = 'table/DELETE_TABLE';
-export const UPDATE_TABLE = 'table/UPDATE_TABLE'; 
+export const DELETE_ITEM = 'table/DELETE_ITEM'; 
+export const UPDATE_TABLE = 'table/UPDATE_TABLE';
