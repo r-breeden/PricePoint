14d9b85f131ffd5e31159f611d43f47f9639d1a9
kiyeonpark <kiyeon.c.park@gmail.com>
2017-09-20 12:31:56 -0700

    change text to payload

--- client/src/store/actions/tables.js
+++ client/src/store/actions/tables.js
@@ -1,10 +1,15 @@
 import * as constants from '../constants/tables';
 
-export const createTable = (input) => ({
+export const createTable = (obj) => ({
   type: constants.CREATE_TABLE,
-  text: input,
+  payload: obj,
 });
 
 export const deleteTable = () => ({
   type: constants.DELETE_TABLE,
+});
+
+export const updateTable = () => ({
+  type: constants.UPDATE_TABLE,
+  text: input,
 });
